43f13d0892ccc3604d1af84387a90953b86c58c4
Allen Shibu <kallenshibu@gmail.com>
2019-03-14 17:40:38 +0530

    Update DateCalculations.java

--- src/DateCalculations.java
+++ src/DateCalculations.java
@@ -1,34 +1,53 @@
-import java.io.File;
 import java.util.Scanner;
 
 public class DateCalculations {
-    public static String year, month, date, dateStr;
+    public static String yearStr, monthStr, dateStr, fullDateStr;
+    public static int year, month, date;
     public static String todaysDate() {
-        //Entering date as seperate elements to add more functonality later
+        //Entering date as separate elements to add more functonality later
         Scanner read = new Scanner( System.in );
 
         System.out.print( "Enter today's year in the foramt YYYY: " );
-        year = read.next();
+        yearStr = read.next();
+        year = Integer.parseInt( yearStr );
         System.out.println();
         System.out.print( "Enter today's month in the format MM: " );
-        month = read.next();
+        monthStr = read.next();
+        month = Integer.parseInt( monthStr );
         System.out.println();
-        System.out.print( "Enter todays date in the format DD: " );
-        date = read.next();
+        System.out.print( "Enter today's date in the format DD: " );
+        dateStr = read.next();
+        date = Integer.parseInt( dateStr );
         System.out.println();
 
-        dateStr = date + month + year;
+        fullDateStr = dateStr + monthStr + yearStr;
 
-        return dateStr;
+        return fullDateStr;
     }
 
     public static int findCurrentTerm() {
-        if( month.equals( "06" ) || month.equals( "07" ) || month.equals( "08" ) ) {
+        if( monthStr.equals( "06" ) || monthStr.equals( "07" ) || monthStr.equals( "08" ) ) {
             return 1;
-        } else if ( month.equals( "09" ) || month.equals( "10" ) || month.equals( "11" ) || month.equals( "12" ) ) {
+        } else if ( monthStr.equals( "09" ) || monthStr.equals( "10" ) || monthStr.equals( "11" ) || monthStr.equals( "12" ) ) {
             return 2;
-        } else if ( month.equals( "01" ) || month.equals( "02" ) || month.equals( "03" ) ) {
+        } else {
             return 3;
         }
+    }
+
+    public static boolean isDueDateOver() {
+        if( findCurrentTerm() == 1 && ( monthStr.equals( "06" ) ||  monthStr.equals( "07" ) ||  monthStr.equals( "08" ) ) ) {
+            return false;
+        } else if( findCurrentTerm() == 2 && ( monthStr.equals( "09" ) ||  monthStr.equals( "10" ) ||  monthStr.equals( "11" ) || monthStr.equals( "12" ) ) ) {
+            return false;
+        } else if( findCurrentTerm() == 3 && ( monthStr.equals( "01" ) ||  monthStr.equals( "02" ) ||  monthStr.equals( "03" ) ) ) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public static int findDaysLate() {
+        return 0;
     }
 }
