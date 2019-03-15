b27223ca79efe8d64ae9cc626fc4e3a241af1274
Allen Shibu <kallenshibu@gmail.com>
2019-03-15 21:28:06 +0530

    Update DateCalculations.java
    Debugging almost over

--- src/DateCalculations.java
+++ src/DateCalculations.java
@@ -1,3 +1,8 @@
+/**
+ * @author Allen Shibu
+ * Class containing functions required to handle date and related stuff for the SchoolFeeManager
+ */
+
 import java.util.Scanner;
 
 public class DateCalculations {
@@ -47,25 +52,34 @@ public class DateCalculations {
         }
     }
 
-    public static boolean isDueDateOver() {
-        if( findCurrentTerm() == 1 && ( monthStr.equals( "06" ) ||  monthStr.equals( "07" ) ||  monthStr.equals( "08" ) ) ) {
+    public static boolean isDueDateOver( int refTerm ) {
+        /**
+         * Function to check whether the due date is over
+         * Number of days late is calculated separately in another function
+         */
+
+        if( refTerm == 1 && ( monthStr.equals( "06" ) ||  monthStr.equals( "07" ) ||  monthStr.equals( "08" ) ) ) {
             return false;
-        } else if( findCurrentTerm() == 2 && ( monthStr.equals( "09" ) ||  monthStr.equals( "10" ) ||  monthStr.equals( "11" ) || monthStr.equals( "12" ) ) ) {
+        } else if( refTerm == 2 && ( monthStr.equals( "09" ) ||  monthStr.equals( "10" ) ||  monthStr.equals( "11" ) || monthStr.equals( "12" ) ) ) {
             return false;
-        } else if( findCurrentTerm() == 3 && ( monthStr.equals( "01" ) ||  monthStr.equals( "02" ) ||  monthStr.equals( "03" ) ) ) {
+        } else if( refTerm == 3 && ( monthStr.equals( "01" ) ||  monthStr.equals( "02" ) ||  monthStr.equals( "03" ) ) ) {
             return false;
         } else {
             return true;
         }
     }
 
-    public static int findDaysLate() {
-        //Yet to be written
-        int daysLate, tempMonth = month - 1;
+    public static int findDaysLate( int refTerm ) {
+        /**
+         * Calculates the number of days late after the last day of the term
+         * Checking whether the due date is over is done separately in another function
+         */
+
+        int daysLate = 0, tempMonth = month - 1;
         if( tempMonth == 0 ) {
             tempMonth = 12;
         }
-        switch( currentTerm ) {
+        switch( refTerm ) {
             case 1:
                 daysLate = date;
                 while ( tempMonth != 8 ) {
@@ -97,16 +111,21 @@ public class DateCalculations {
                 }
                 break;
         }
+        return daysLate;
     }
 
     public static int findDaysInMonth( int monthToFind ) {
+        /**
+         * Finds and returns the number of days in a month and returns it
+         * This function is used occasionally in the program
+         */
         if( monthToFind == 1 || monthToFind == 3 || monthToFind == 7 || monthToFind == 8 || monthToFind == 10 || monthToFind == 12 ) {
             return 31;
         } else if( monthToFind == 4 || monthToFind == 6 || monthToFind == 9 || monthToFind == 11 ) {
             return 30;
         } else if ( monthToFind == 2 && year % 4 == 0 ) {
             return 29;
-        } else if ( monthToFind == 2 &&  year % 4 != 0 ) {
+        } else { //if ( monthToFind == 2 &&  year % 4 != 0 ) {
             return 28;
         }
     }
